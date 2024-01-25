d3eed29484f8b4c67b842d1caf14fb3ef27c5fe2
Tearever <trevor.wright17@yahoo.com>
2024-01-25 16:27:29 -0600

    merge

--- gamble.py
+++ gamble.py
@@ -3,10 +3,7 @@
 import random
 
 
-list1 = [1, 2 , 3 , 4, 5, 6]
-#not sure what is happening
-list2 = [1, 2 , 3 , 4, 5, 6]
-#test
+list2 = [1, 2 , 3 , 4, 5, 6, 7, 8, 9, 10]
 
 
 print(random.choice(list2))
